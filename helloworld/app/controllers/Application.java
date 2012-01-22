0cec1d8c3b58b02a910a42c19c854683d716a566
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-01-22 21:32:34 +0100

    Hello world done

--- helloworld/app/controllers/Application.java
+++ helloworld/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render("Your new application is ready."));
+      String message = "Hello World";
+      return ok( index.render(message) );
   }
-  
 }
